061a74a7e6f0d09aa7dba9bf9c693b25eed68a7e
acanha <canha.andre@gmail.com>
2020-10-13 18:02:30 +0100

    solution to problem 14980

---
+++ Easy/1480-RunningSumOf1dArray.cs
@@ -0,0 +1,20 @@
+﻿public class Problem1480
+{
+    /*
+        Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+        Return the running sum of nums.
+     */
+    public int[] Solution(int[] nums)
+    {
+        var result = new int[nums.Length];
+        result[0] = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = result[i - 1] + nums[i];
+        }
+
+        return result;
+    }
+}
+

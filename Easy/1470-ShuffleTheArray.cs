c92d6a1c4a3f9775adcb1edd753efd3254a9fd58
acanha <canha.andre@gmail.com>
2020-10-17 18:21:41 +0100

    solution to problem 1470

---
+++ Easy/1470-ShuffleTheArray.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ */
+
+public class Problem1470
+{
+    public int[] Solution(int[] nums, int n)
+    {
+        var result = new int[2 * n];
+        var index = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            result[index] = nums[i];
+            index++;
+            result[index] = nums[i + n];
+            index++;
+        }
+
+        return result;
+    }
+}
+

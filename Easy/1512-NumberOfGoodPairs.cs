21946b0594fb757adf05381d1227c33ed9657dcc
acanha <canha.andre@gmail.com>
2020-10-19 19:15:49 +0100

    solution to problem 1512

---
+++ Easy/1512-NumberOfGoodPairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+    Given an array of integers nums.
+    A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+    Return the number of good pairs.
+ 
+ */
+public class Problem1512
+{
+    public int Solution(int[] nums)
+    {
+        var numberOfGoodPairs = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                    numberOfGoodPairs++;
+
+            }
+        }
+
+        return numberOfGoodPairs;
+    }
+}
+

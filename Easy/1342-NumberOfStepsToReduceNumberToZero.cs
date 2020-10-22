4f5e2eb91ed0649382b112503ba1b4c45f425957
acanha <canha.andre@gmail.com>
2020-10-22 21:52:43 +0100

    solution to problem 1342

---
+++ Easy/1342-NumberOfStepsToReduceNumberToZero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+    Given a non-negative integer num, return the number of steps to reduce it to zero.
+    If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+ */
+
+public class Problem1342
+{
+    public int Solution(int num)
+    {
+        return NumberSteps(num, 0);
+    }
+
+    private bool IsEven(int number) => number % 2 == 0;
+
+    private int NumberSteps(int number, int result)
+    {
+        if (number != 0)
+        {
+            result = IsEven(number) ? NumberSteps(number / 2, ++result) :NumberSteps(number - 1, ++result); 
+        }
+
+        return result;
+    }
+}
+

f258fe2a28b94e3a17b7e663ec43ded08d6544aa
acanha <canha.andre@gmail.com>
2020-10-24 18:45:00 +0100

    solution to problem 1365

---
+++ Easy/1365-HowManyNumbersAreSmallerThanCurrentNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+    Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. 
+    That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+    Return the answer in an array.
+ */
+
+public class Problem1365
+{
+    public int[] Solution(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var number = nums.ToList().ElementAt(i);
+            var smallerThanNumberCount = nums.Where(x => x != number && x < number).Count();
+            result[i] = smallerThanNumberCount;
+        }
+
+        return result;
+    }
+}
+

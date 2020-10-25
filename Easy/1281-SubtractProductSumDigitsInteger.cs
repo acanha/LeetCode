dc46a73fb5cb7e452561ac271ad2343c5d47d78c
acanha <canha.andre@gmail.com>
2020-10-25 11:09:40 +0000

    solution to problem 1281

---
+++ Easy/1281-SubtractProductSumDigitsInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+using System.Text;
+
+/*
+    Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ */
+public class Problem1281
+{
+    public int Solution(int n)
+    {
+        var product = 1;
+        var sum = 0;
+
+        while (n > 0)
+        {
+            var number = n % 10;
+            product = product * number;
+            sum = sum + number;
+            n = n / 10;
+        }
+        
+        return product - sum;
+    }
+}
+
+

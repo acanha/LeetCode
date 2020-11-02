605a014e0719172273435665d4cb380eb15e34a3
acanha <canha.andre@gmail.com>
2020-11-02 19:36:12 +0000

    solution to problem 1290

---
+++ Easy/1290-ConvertBinaryNumberLinkedListToInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+    Given head which is a reference node to a singly-linked list. 
+    The value of each node in the linked list is either 0 or 1. 
+    The linked list holds the binary representation of a number.
+
+    Return the decimal value of the number in the linked list.
+ */
+
+public class Problem1290
+{
+    public int Solution(ListNode head)
+    {
+        var number = GenerateBinary(head);
+
+        return Convert.ToInt32(number, 2);
+    }
+
+    private string GenerateBinary(ListNode node)
+        => $"{node.val}{(node.next != null ? GenerateBinary(node.next) : string.Empty)}";
+    
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+

7dc217f4a0c92f7baa4349032ccf94aa27a330ca
acanha <canha.andre@gmail.com>
2020-11-03 21:17:22 +0000

    solution to problem 1295

---
+++ Easy/1295-FindNumbersWithEvenNumberDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+public class Problem1295
+{
+    public int Solution(int[] nums)
+    {
+        var result = 0;
+
+        foreach (int number in nums)
+        {
+            var digits = Math.Floor(Math.Log10(number) + 1);
+
+            if (digits % 2 == 0)
+                result++;
+        }
+
+        return result;
+    }
+}
+

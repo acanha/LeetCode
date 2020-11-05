31a271ebe923aa78c474e1ee90c3c4cfc9d7b4cf
acanha <canha.andre@gmail.com>
2020-11-05 20:00:32 +0000

    solution to problem 709

---
+++ Easy/709-ToLowerCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+public class Problem709
+{
+    public string Solution(string str)
+    {
+        return str.ToLower();
+    }
+}
+

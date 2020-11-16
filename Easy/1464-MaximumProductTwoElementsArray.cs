37b5c7df41ee1243fff7a6f5a5e82e35c23df3bb
acanha <canha.andre@gmail.com>
2020-11-16 21:30:30 +0000

    solution to problem 1464

---
+++ Easy/1464-MaximumProductTwoElementsArray.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Given the array of integers nums, you will choose two different indices i and j of that array. 
+    Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ */
+using System.Linq;
+
+public class Problem1464
+{
+    public int Solution(int[] nums)
+    {
+        var lastTwo = nums.OrderBy(number => number)
+                          .TakeLast(2)
+                          .ToArray();
+
+        return (lastTwo[0] - 1) * (lastTwo[1] - 1);
+
+    }
+}
+

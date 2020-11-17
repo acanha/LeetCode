095c4a9f48a38ae14734488edb4afea3f4421e82
acanha <canha.andre@gmail.com>
2020-11-17 17:41:10 +0000

    solution to problem 905

---
+++ Easy/905-SortArrayByParity.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Given an array A of non-negative integers, return an array consisting of all the even elements of A, 
+    followed by all the odd elements of A.
+
+    You may return any answer array that satisfies this condition.
+ */
+
+using System.Linq;
+
+public class Problem905
+{
+    public int[] Solution(int[] A)
+    {
+        var evenNumbers = A.Where(number => number % 2 == 0);
+        var oddNumbers = A.Where(number => number % 2 != 0);
+
+        return evenNumbers.Concat(oddNumbers).ToArray();
+    }
+}
+

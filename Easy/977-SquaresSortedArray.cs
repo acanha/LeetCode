5581715c7f2f3eb52ac64002801488e2dd7a5876
acanha <canha.andre@gmail.com>
2020-11-18 18:57:53 +0000

    solution to problem 977

---
+++ Easy/977-SquaresSortedArray.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Given an array of integers A sorted in non-decreasing order, 
+    return an array of the squares of each number, also in sorted non-decreasing order.
+ */
+using System;
+using System.Linq;
+
+public class Problem977
+{
+    public int[] Solution(int[] A)
+    {
+        var result = new int[A.Length];
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            result[i] =(int)Math.Pow(A[i], 2);
+        }
+
+        return result.OrderBy(number => number).ToArray();
+    }
+}
+

f911d6e61fefdd00ebaab30d0e16590095bcce0e
acanha <canha.andre@gmail.com>
2020-11-22 23:25:12 +0000

    solution to problem 961

---
+++ Easy/961-N-RepeatedElementInSize2NArray.cs
@@ -0,0 +1,18 @@
+﻿/*
+    In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+    Return the element repeated N times.
+ */
+
+using System.Linq;
+
+public class Problem961
+{
+    public int Solution(int[] A)
+    {
+        return A.GroupBy(number => number)
+                .First(number => number.Count() == A.Length / 2)
+                .Key;
+    }
+}
+

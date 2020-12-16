3731667289abe13fe1accb8614b720c420f041b0
acanha <canha.andre@gmail.com>
2020-12-16 21:20:28 +0000

    solution to problem 1684

---
+++ Easy/1684-CountNumberConsistentStrings.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+public class Problem1684
+{
+    public int Solution(string allowed, string[] words)
+    {
+        int result = 0;
+        var characters = allowed.Select(c => c.ToString());
+
+        foreach (var word in words)
+        {
+            var w = word;
+            foreach (var charecter in characters)
+            {
+                w = w.Replace(charecter, string.Empty);
+            }
+
+            result = w.Length > 0 ? result : ++result;
+        }
+
+        return result;
+    }
+}
+

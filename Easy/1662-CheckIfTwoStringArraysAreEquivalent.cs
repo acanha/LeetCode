791174abf67aed5c320842f23bba5937b5ad36f5
acanha <canha.andre@gmail.com>
2020-12-17 19:38:40 +0000

    solution to problem 1662

---
+++ Easy/1662-CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+public class Problem1662
+{
+    public bool Solution(string[] word1, string[] word2)
+    {
+        var firstWord = word1.Aggregate((actual, next) => actual + next);
+        var secondWord = word2.Aggregate((actual, next) => actual + next);
+        return firstWord.Equals(secondWord);
+    }
+}
+

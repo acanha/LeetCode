4460be1446b8c5e57180d0f7b07b0a4de68ab401
acanha <canha.andre@gmail.com>
2024-02-25 18:13:36 +0000

    solution to problem 242

---
+++ Easy/String/242-ValidAnagram.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+    typically using all the original letters exactly once.
+ */
+using System.Linq;
+
+public class Problem242
+{
+    public bool Solution(string s, string t)
+    {
+        var firstStringOrdered = s.Order();
+        var secondStringOrdered = t.Order();
+
+        return firstStringOrdered.SequenceEqual(secondStringOrdered);
+    }
+}
+

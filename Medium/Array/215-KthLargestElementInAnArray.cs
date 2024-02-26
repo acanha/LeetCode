2ae8093f9e6222567561c5f252e139749111c049
acanha <canha.andre@gmail.com>
2024-02-26 17:09:50 +0000

    solution to problem 215

---
+++ Medium/Array/215-KthLargestElementInAnArray.cs
@@ -0,0 +1,33 @@
+﻿/*
+    Given an integer array nums and an integer k, return the kth largest element in the array.
+
+    Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+    Can you solve it without sorting?
+ */
+using System.Diagnostics;
+using System.Linq;
+
+public class Problem215
+{
+    public int Solution(int[] nums, int k)
+    {
+        return nums.ToList()
+                   .OrderDescending()
+                   .ElementAt(k - 1);
+        
+    }
+
+    public int Solution2(int[] nums, int k)
+    {
+        var list = nums.ToList();
+
+        for (int i = 0; i < k - 1; i++)
+        {
+            list.Remove(list.Max());
+        }
+
+        return list.Max();
+    }
+}
+

08158149ab594b06704c7edd6e363b2a2dac8665
acanha <canha.andre@gmail.com>
2024-02-27 18:18:11 +0000

    solution to problem 101

--- Easy/617-MergeTwoBinaryTrees.cs
+++ Easy/617-MergeTwoBinaryTrees.cs
@@ -85,5 +85,11 @@ public class TreeNode
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x) { val = x; }
+
+    public TreeNode(int value,TreeNode left = null, TreeNode right = null) 
+    { 
+        this.val = value; 
+        this.left  = left;
+        this.right = right;
+    }
 }
---
+++ Easy/Tree/101-SymmetricTree.cs
@@ -0,0 +1,78 @@
+﻿/*
+    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+public class Problem101
+{
+    private List<int> LeftSide = new List<int>();
+    private List<int> RightSide = new List<int>();
+
+    public bool Solution(TreeNode root)
+    {
+        if (root.left != null)
+        {
+            LeftSide.Add(root.left.val);
+            GetLeftSide(root.left);
+        }
+
+        if (root.right != null)
+        {
+            RightSide.Add(root.right.val);
+            GetRightSide(root.right);
+        }
+
+        return LeftSide.SequenceEqual(RightSide);
+    }
+
+    private List<int> GetLeftSide(TreeNode root)
+    {
+        if (root.left != null)
+        {
+            LeftSide.Add(root.left.val);
+            GetLeftSide(root.left);
+        }
+        else
+        {
+            LeftSide.Add(0);
+        }
+
+        if (root.right != null)
+        {
+            LeftSide.Add(root.right.val);
+            GetLeftSide(root.right);
+        }
+        else
+        {
+            LeftSide.Add(0);
+        }
+
+        return LeftSide;
+    }
+
+    private List<int> GetRightSide(TreeNode root)
+    {
+        if (root.right != null)
+        {
+            RightSide.Add(root.right.val);
+            GetRightSide(root.right);
+        }
+        else
+        {
+            RightSide.Add(0);
+        }
+
+        if (root.left != null)
+        {
+            RightSide.Add(root.left.val);
+            GetRightSide(root.left);
+        }
+        else
+        {
+            RightSide.Add(0);
+        }
+
+        return RightSide;
+    }
+}

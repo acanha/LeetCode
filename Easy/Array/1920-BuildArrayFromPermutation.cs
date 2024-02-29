b739389a04c93b8c6637662e60342db988eacff3
acanha <canha.andre@gmail.com>
2024-02-29 17:41:11 +0000

    solution to problem 1920

---
+++ Easy/Array/1920-BuildArrayFromPermutation.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+    A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+ */
+public class Problem1920
+{
+    public int[] Solution(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+}
+

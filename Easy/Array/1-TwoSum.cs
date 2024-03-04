0d97aa424269c6aab90531e7cca40fd3c3bab882
acanha <canha.andre@gmail.com>
2024-03-04 15:03:38 +0000

    solution to problem 1

---
+++ Easy/Array/1-TwoSum.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice. 
+
+     You can return the answer in any order.
+     
+ */
+using System.Collections.Generic;
+
+public class Problem1
+{
+    public int[] Solution(int[] nums, int target)
+    {
+        var indices = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    indices.Add(i);
+                    indices.Add(j);
+                    break;
+                }
+            
+            }
+
+            if (indices.Count != 0)
+                break;
+        }
+
+        return indices.ToArray();
+    }
+}
+

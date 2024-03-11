7f84d74191f949a93fdcabd0ad0b1fd63e2bc6df
acanha <canha.andre@gmail.com>
2024-03-11 23:07:51 +0000

    solution to problem 125

---
+++ Easy/Two Pointers/125-ValidPalindrome.cs
@@ -0,0 +1,46 @@
+﻿/*
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+    it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+    Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+
+using System.Text;
+
+public class Problem125
+{
+    public bool Solution(string s)
+    {
+        var isPalindrome = true;
+
+        var builder = new StringBuilder();
+
+        foreach (var word in s)
+        {
+            if (char.IsAsciiLetterOrDigit(word))
+            {
+                builder.Append(char.ToLower(word));
+            }
+        }
+
+        var cleaned = builder.ToString();
+        var middle = cleaned.Length / 2;
+
+        if(cleaned.Length == 1)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < middle; i++)
+        {
+            if(cleaned[i] != cleaned[cleaned.Length - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        return isPalindrome;
+    }
+}
+

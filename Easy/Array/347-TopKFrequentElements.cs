37beb5ac3908dca4cb7cea240a97b1d63df06739
acanha <canha.andre@gmail.com>
2024-03-25 14:28:49 +0000

    solution to problem 347

---
+++ Easy/Array/347-TopKFrequentElements.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Given an integer array nums and an integer k, return the k most frequent elements. 
+    You may return the answer in any order.
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+public class Problem347
+{
+    public int[] Solution(int[] nums, int k)
+    {
+        var mostFrequentElements = new Dictionary<int, int>();
+
+        foreach (var element in nums)
+        {
+            mostFrequentElements[element] = mostFrequentElements.ContainsKey(element)
+                                            ? mostFrequentElements[element] += 1
+                                            : 1;
+        }
+
+        return mostFrequentElements.OrderByDescending(x => x.Value)
+                                   .Take(k)
+                                   .Select(x => x.Key)
+                                   .ToArray();
+    }
+}
